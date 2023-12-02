12ddc4b04ba4860a873c13202061d7a882e57535
junghyeonsu <jung660317@naver.com>
2023-12-02 22:58:22 +0900

    feat: 여러 로고들 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-const logoSvg512 = "/logo512.svg";
-const logoPng512 = "/logo512.png";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,20 +11,31 @@ export default defineConfig({
     VitePWA({
       filename: "킹모장",
       registerType: "autoUpdate",
-      includeAssets: [logoSvg512],
+      includeAssets: ["robots.txt"],
       manifest: {
         theme_color: "#ffffff",
         icons: [
           {
-            src: logoSvg512,
+            src: "/logo-x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "/logo-x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo-x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/maskable-logo-x512.png",
             sizes: "512x512",
             type: "image/svg+xml",
             purpose: "any maskable",
-          },
-          {
-            src: logoPng512,
-            sizes: "512x512",
-            type: "image/png",
           },
         ],
       },
