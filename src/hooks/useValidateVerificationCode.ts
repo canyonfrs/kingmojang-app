597e9d69eb4dbedc860fed78997def7e01482a4b
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2023-12-16 17:36:03 +0900

    feat: useValidateVerificationCode 구현

---
+++ src/hooks/useValidateVerificationCode.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { get } from "../remotes";
+
+const PATH = "/members/validate-verification-code";
+const QUERY_KEY = "validate-verification-code";
+
+async function validateVerificationCode(code: string) {
+  return get<[]>(PATH, {
+    headers: {
+      Authorization: `Bearer ${code}`,
+    },
+  });
+}
+
+export function useValidateVerificationCode(code: string) {
+  return useQuery([QUERY_KEY, code], () => validateVerificationCode(code));
+}
