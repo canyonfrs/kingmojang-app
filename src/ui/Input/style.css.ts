fb98c4b8bf67726b7902ccfdd563365171970fd9
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2023-12-16 17:05:59 +0900

    feat: UI/Input

---
+++ src/ui/Input/style.css.ts
@@ -0,0 +1,19 @@
+import { style } from "@vanilla-extract/css";
+
+export const input = style({
+  width: "100%",
+  height: "100%",
+
+  fontSize: "16px",
+
+  padding: "6px",
+
+  backgroundColor: "#FFFFFF",
+
+  border: "1px solid #7A7A7A",
+  outline: "none",
+
+  ":focus": {
+    border: "1px solid #0078D7",
+  },
+});
