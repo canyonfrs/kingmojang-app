abf5a6aec873c0ffe83126dd0eb13ef957729034
= <=>
2023-12-17 16:19:54 +0900

    feat: 글자크기 변경, 현재 행열 위치 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
+
 import * as Styled from "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import type { Coordinate } from "./hooks/useTextarea";
 import { Textarea } from "./ui/Textarea";
 
 function App() {
+  const [position, setPosition] = useState<Coordinate>({ column: 0, row: 0 });
   return (
     <div className={Styled.container}>
       <Header />
-      <Textarea />
-      <Footer />
+      <Textarea setPosition={setPosition} />
+      <button>클릭</button>
+      <Footer position={position} />
     </div>
   );
 }
